302b189c2a4355c79181527270780cf6a40c770b
dima0104 <101921625+dima0104@users.noreply.github.com>
2026-08-04 14:10:33 +0200

    "Add Gui"

---
+++ gui.py
@@ -0,0 +1,12 @@
+import customtkinter as ctk
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
+
+app = ctk.CTk()
+
+app.title("Employee Management System")
+
+app.geometry("1000x500")
+
+app.mainloop()
